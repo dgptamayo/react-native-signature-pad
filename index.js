fd0e29647845bae7ab5b10e168d3d269b13954e7
Domingo Tamayo <dgptamayo@gmail.com>
2018-01-23 23:13:27 +1100

    Bugfix proptypes

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ class SignaturePad extends Component {
   static propTypes = {
     onChange: PropTypes.func,
     onError: PropTypes.func,
-    style: View.propTypes.style,
     penColor: PropTypes.string,
     dataURL: PropTypes.string,
   };
